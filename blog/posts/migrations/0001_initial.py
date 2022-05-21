f7aa62e88548a4eded7caa9b9313662e24edcf5e
Jakub Samul <jakubsamul0@gmail.com>
2022-05-21 10:55:55 +0200

    black

--- blog/posts/migrations/0001_initial.py
+++ blog/posts/migrations/0001_initial.py
@@ -15,20 +15,42 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name="Post",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('active', models.BooleanField(default=False, verbose_name='aktywny')),
-                ('pub_date', models.DateTimeField(blank=True, null=True, verbose_name='data ppublikacji')),
-                ('title', models.CharField(max_length=25, verbose_name='tytuł')),
-                ('slug', models.SlugField()),
-                ('lead', models.TextField(verbose_name='wprowadzenie')),
-                ('body', models.TextField(verbose_name='treść posta')),
-                ('author', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, verbose_name='autor')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("active", models.BooleanField(default=False, verbose_name="aktywny")),
+                (
+                    "pub_date",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="data ppublikacji"
+                    ),
+                ),
+                ("title", models.CharField(max_length=25, verbose_name="tytuł")),
+                ("slug", models.SlugField()),
+                ("lead", models.TextField(verbose_name="wprowadzenie")),
+                ("body", models.TextField(verbose_name="treść posta")),
+                (
+                    "author",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="autor",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'wpis',
-                'verbose_name_plural': 'wpisy',
+                "verbose_name": "wpis",
+                "verbose_name_plural": "wpisy",
             },
         ),
     ]
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -6,13 +6,15 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     active = models.BooleanField(verbose_name="aktywny", default=False)
     pub_date = models.DateTimeField(
-        verbose_name="data ppublikacji", null=True, blank=True)
+        verbose_name="data ppublikacji", null=True, blank=True
+    )
     title = models.CharField(max_length=25, verbose_name="tytuł")
     slug = models.SlugField()
     lead = models.TextField(verbose_name="wprowadzenie")
     body = models.TextField(verbose_name="treść posta")
     author = models.ForeignKey(
-        User, verbose_name="autor", null=True, blank=True, on_delete=models.CASCADE)
+        User, verbose_name="autor", null=True, blank=True, on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = "wpis"
